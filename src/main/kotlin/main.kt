83855eb2709da9c5663a79494e5df8e4a105e257
Pavel Vinogradov <mr.friderick@ya.ru>
2024-01-05 10:53:56 +0300

    Добавлена логика для подсчета скидки

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,17 @@
 fun main() {
+    val amount          = 2000
+    val regularСustomer = true
+    var discount        = 0
 
+    if (amount >= 1001 && amount <= 10000) {
+        discount = 100
+    } else if (amount >= 10001) {
+        discount = (amount * 0.05).toInt()
+    }
+
+    if (regularСustomer) {
+        discount += ((amount - discount) * 0.01).toInt()
+    }
+
+    println("Сумма заказа: $amount. Скидка составляет: $discount")
 }
